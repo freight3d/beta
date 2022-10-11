3367c4e9acd48e0a99965c6f008cc73124002829
rodrigo826 <58783821+rodrigo826@users.noreply.github.com>
2022-10-11 17:52:11 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -114,7 +114,12 @@ function create_piece(){
 
 // Create Cylinder
 function create_piece2(){
-	const material = new THREE.MeshPhongMaterial({color: 0x797979,shininess: 100});
+	const loaderTexture = new THREE.TextureLoader();
+	loaderTexture.load('images/metal.jpg', (texture) => {
+	const material = new THREE.MeshBasicMaterial({
+		map: texture,
+		});
+	//const material = new THREE.MeshPhongMaterial({color: 0x797979,shininess: 100});
         let geometry;
         if(document.getElementsByName("units")[0].checked){
 			geometry = new THREE.CylinderGeometry(document.getElementsByName("width")[0].value/200,document.getElementsByName("width")[0].value/200,document.getElementsByName("length")[0].value/100,32);
